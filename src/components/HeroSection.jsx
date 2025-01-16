8d4abe79092df48cf10bdce8e2720459cfeb02d2
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-01-16 04:14:14 +0100

    Add HeroSection component

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,14 @@
+const HeroSection = () => {
+    return (
+        <div className="hero__content">
+            <h1>Building Innovative <span>Full Stack </span>Solutions</h1>
+            <p>Transforming Your Ideas into Reality With Modern Web Development Technologies</p>
+            <div className="button-section">
+                <a href="#contact" className="btn-primary">Let's Talk</a>
+                <a href="#work" className="link-secondary">My Work</a>
+            </div>
+        </div>
+    );
+};
+
+export {HeroSection};
