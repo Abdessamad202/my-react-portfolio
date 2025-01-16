465136caba92936ee765b874efb9b8aae892d651
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-01-16 04:14:18 +0100

    Add navigation data

---
+++ src/data/navData.js
@@ -0,0 +1,7 @@
+const navItems = [
+    { path: "/", icon: "fas fa-home", tooltip: "Home" },
+    { path: "/tools", icon: "fas fa-wrench", tooltip: "Tools" },
+    { path: "/projects", icon: "fas fa-folder", tooltip: "Projects" },
+    { path: "/contact", icon: "fas fa-envelope", tooltip: "Contact" },
+];
+export {navItems};
