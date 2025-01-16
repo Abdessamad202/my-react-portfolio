80043dc095f9d01ecead668a4e366bd0a7b1ba09
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-01-16 04:14:15 +0100

    Add Infos component

---
+++ src/components/Infos.jsx
@@ -0,0 +1,11 @@
+import {HeroSection} from "./HeroSection.jsx";
+
+const Infos = () => {
+    return (
+        <div className="col-lg-8 absolute cln mt-100 mt-lg-120">
+            <HeroSection />
+        </div>
+
+    )
+}
+export {Infos}
