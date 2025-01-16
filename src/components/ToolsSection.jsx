63325c2709f7e7bc2f019fd4db230f710cb8a2a4
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-01-16 13:02:33 +0100

    Fix bug in ToolsSection component: [Describe the fix]

--- src/components/ToolsSection.jsx
+++ src/components/ToolsSection.jsx
@@ -5,7 +5,7 @@ const ToolsSection = () => {
             <h1 className="header">My Developer Toolkit</h1>
             <div className="row">
                 {tools.map((tool, index) => (
-                    <div key={index} className="col-md-4 col-6">
+                    <div key={index} className="col-md-4 col-sm-6">
                         <div className="card">
                             <img src={tool.imgSrc} alt={tool.alt} className="img-fluid" />
                             <h3>{tool.title}</h3>
