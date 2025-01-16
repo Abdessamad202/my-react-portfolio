40786b1282c22143597e17387ed083bc138b3186
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-01-16 04:14:18 +0100

    Add project data

---
+++ src/data/projectData.js
@@ -0,0 +1,7 @@
+const projects = [
+    { title: "Project 1", subtitle: "Creative Portfolio Template", className: "stabraq-bg" },
+    { title: "Project 2", subtitle: "Modern Business Template", className: "najmai-bg" },
+    { title: "Project 3", subtitle: "Task Management Template", className: "taskai-bg" },
+    { title: "Project 4", subtitle: "SaaS Landing Page Template", className: "ruya-bg" },
+];
+export {projects};
