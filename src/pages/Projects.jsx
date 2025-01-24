fd199a727867dcf6f958208d4187fedcfa16ea60
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-01-24 01:44:30 +0100

    Updated Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,22 +1,12 @@
-import {Navbar} from "../components/Navbar.jsx";
-import {Profile} from "../components/Profile.jsx";
-import {CollaborateCard} from "../components/CollaborateCard.jsx";
-import {Templates} from "../components/Templates.jsx";
+import { CollaborateCard } from "../components/CollaborateCard.jsx";
+import { Templates } from "../components/Templates.jsx";
 
 const Projects = () => {
     return (
         <>
-            <Navbar/>
-            <div className="container ">
-                <div className="row ">
-                    <Profile />
-                    <div className="col-lg-8 absolute cln mt-100 mt-lg-120">
-                        <Templates />
-                        <CollaborateCard />
-                    </div>
-                </div>
-            </div>
+            <Templates />
+            <CollaborateCard />
         </>
-    )
-}
-export  {Projects};
+    );
+};
+export { Projects };
