5a03ba5f830f75a5f1d7a9c291b4652229a142d2
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-01-24 01:44:39 +0100

    Updated Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,17 @@
-import {Navbar} from "../components/Navbar.jsx";
-import {Profile} from "../components/Profile.jsx";
-import {HeroSection} from "../components/HeroSection.jsx";
-import {ToolsSection} from "../components/ToolsSection.jsx";
-import {CollaborateCard} from "../components/CollaborateCard.jsx";
+// import { Navbar } from "../components/Navbar.jsx";
+// import { Profile } from "../components/Profile.jsx";
+import { HeroSection } from "../components/HeroSection.jsx";
+import { ToolsSection } from "../components/ToolsSection.jsx";
+import { CollaborateCard } from "../components/CollaborateCard.jsx";
+// import { Wrapper } from "../components/Content.jsx";
 
 const Home = () => {
     return (
         <>
-            <Navbar/>
-            <div className="container ">
-                <div className="row ">
-                    <Profile />
-                    <div className="col-lg-8 absolute cln mt-100 mt-lg-120">
-                        <HeroSection/>
-                        <ToolsSection />
-                        <CollaborateCard />
-                    </div>
-                </div>
-            </div>
+            <HeroSection />
+            <ToolsSection />
+            <CollaborateCard />
         </>
-    )
-}
-export {Home}
+    );
+};
+export { Home };
