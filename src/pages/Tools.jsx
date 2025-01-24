e918eee628181a4b2df913890da097e9bdda0fa2
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-01-24 01:44:21 +0100

    Updated Tools.jsx

--- src/pages/Tools.jsx
+++ src/pages/Tools.jsx
@@ -1,22 +1,12 @@
-import {Navbar} from "../components/Navbar.jsx";
-import {Profile} from "../components/Profile.jsx";
-import {ToolsSection} from "../components/ToolsSection.jsx";
-import {CollaborateCard} from "../components/CollaborateCard.jsx";
+import { ToolsSection } from "../components/ToolsSection.jsx";
+import { CollaborateCard } from "../components/CollaborateCard.jsx";
 
 const Tools = () => {
     return (
         <>
-            <Navbar/>
-            <div className="container ">
-                <div className="row ">
-                    <Profile />
-                    <div className="col-lg-8 absolute cln mt-100 mt-lg-120">
-                        <ToolsSection />
-                        <CollaborateCard />
-                    </div>
-                </div>
-            </div>
+            <ToolsSection />
+            <CollaborateCard />
         </>
-    )
-}
-export  {Tools};
+    );
+};
+export { Tools };
